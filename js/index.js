5165b4b17bf93c00cb2a2b794363c266e2c57976
melissa-murphy <melissamurphy.ca@gmail.com>
2019-04-09 15:19:21 -0700

    Still working through various even listeners

--- js/index.js
+++ js/index.js
@@ -1,28 +1,60 @@
 // on scroll(on wheel?) shrink nav height
-    // bonus: zoom image of bus up to brand
+// bonus: zoom image of bus up to brand
+const fixedHeader = document.querySelector('header');
 
-
+document.addEventListener('scroll', (event) => {
+    if (fixedHeader < 50) {
+        fixedHeader.style.height = '5rem';
+        fixedHeader.style.transition = 'all 2s';
+    } else {
+        fixedHeader.style.height = '4rem';
+        fixedHeader.style.transition = 'all 2s';
+    }
+});
 
 // on load intro h2 animated bounce in
-    //animationstart + animationend
+//animationstart + animationend
+//onload.addEventListener()
 
 
 
 // resize, alert('Ouch! Just kidding. I'm just a window.')
+window.addEventListener('resize', (event) => {
+    alert('Ouch! Just kidding, I am a window.');
+})
 
 
 // mouseenter, button bump up, text change to 'haha, missed me!', then revert. only 1x per button
 
 
 
-// mouseover, images bounce, etc.
+// [need replacement], images bounce, etc.
 
 
 
 // drag, alert('Zooooom!)
-    // dragend to remove alert
+// dragend to remove alert
+window.addEventListener('onwheel', (event) => {
+    alert('ZOOOOOOOOOOOOOM');
+
+    //reset
+    setTimeout(function() {
+        alert = '';
+    }, 3000);
+}, false);
 
 
+// mouseover, text-content h2, dropshadow fades in
+const h2Shadow = document.querySelector('h2');
 
-// focus, text-content h2, dropshadow fades in
-    // blur, fades out on defocus
+h2Shadow.addEventListener('mouseover', (event) => {
+    event.target.style.textShadow = '3px 3px 5px black';
+    event.target.style.transition = 'all 1s';
+
+    // reset
+    setTimeout(function() {
+        event.target.style.textShadow = '';
+    }, 3000);
+}, false);
+
+
