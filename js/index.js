03f93e470f49e75c4c37a9c65b3fb97b678a3191
melissa-murphy <melissamurphy.ca@gmail.com>
2019-04-09 12:36:41 -0700

    Sketching out event ideas

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-// Your code goes here
+// on scroll(on wheel?) shrink nav height
+    // bonus: zoom image of bus up to brand
+
+
+
+// on load intro h2 animated bounce in
+    //animationstart + animationend
+
+
+
+//
