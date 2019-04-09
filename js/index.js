27e1d043444cecc91dec204f87489eb034b12144
melissa-murphy <melissamurphy.ca@gmail.com>
2019-04-09 12:50:54 -0700

    11 planned event listeners and actions described

--- js/index.js
+++ js/index.js
@@ -8,4 +8,21 @@
 
 
 
-//
+// resize, alert('Ouch! Just kidding. I'm just a window.')
+
+
+// mouseenter, button bump up, text change to 'haha, missed me!', then revert. only 1x per button
+
+
+
+// mouseover, images bounce, etc.
+
+
+
+// drag, alert('Zooooom!)
+    // dragend to remove alert
+
+
+
+// focus, text-content h2, dropshadow fades in
+    // blur, fades out on defocus
